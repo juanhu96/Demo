06b5dcf54ffa1aeda927d660f3daedd4f929568f
Li Zhijian <zhijianli9999@gmail.com>
2023-05-21 21:56:05 -0700

    update/fix tols

--- pyblp_demest.py
+++ pyblp_demest.py
@@ -2,7 +2,6 @@ import pandas as pd
 import pyblp
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
 poolnum = 24
 
@@ -17,7 +16,8 @@ formulation1 = pyblp.Formulation('1 + prices + log(dist) + race_black + race_asi
 formulation2 = pyblp.Formulation('0 + log(dist)')
 product_formulation = (formulation1, formulation2)
 
-integ_config = pyblp.Integration('monte_carlo', size=50, specification_options={'seed': 0})
+integ_config = pyblp.Integration('monte_carlo', size=100, specification_options={'seed': 0})
+opt_config = pyblp.Optimization('trust-constr', {'gtol': 1e-10})
 
 problem = pyblp.Problem(product_formulations=product_formulation, product_data=df, integration=integ_config)
 
@@ -29,38 +29,41 @@ print(problem)
 
 
 re_estimate = True #Switch to False to read the pickle
-picklepath = f"{datadir}/Analysis/res_byhpi.pkl"
+picklepath = f"{datadir}/Analysis/res_pyblp.pkl"
 if re_estimate:
     with pyblp.parallel(poolnum):
-        res = problem.solve(sigma = 0.1, optimization=pyblp.Optimization('trust-constr'))
+        res = problem.solve(sigma = 0.1, optimization=opt_config)
     res.to_pickle(picklepath)
 else:
     res = pyblp.read_pickle(picklepath)
 
 print(res)
-print('objective value:', res.objective)
-for i in range(len(res.beta)):
-    print(res.beta_labels[i], res.beta[i])
-print('sigma:', res.sigma)
-# TODO: check if we have dist in X1 in the PE paper
+print('\nBeta:')
+for ii in range(len(res.beta)):
+    print(res.beta_labels[ii], res.beta[ii])
+print('\nSigma:')
+for ii in range(len(res.sigma)):
+    print(res.sigma_labels[ii], res.sigma[ii])
 
 
-de = res.compute_elasticities('dist')
 # summary stats
-print("Summary stats of elasticities")
-print(pd.Series(de.flatten()).describe())
+de = res.compute_elasticities('dist')
+df['dist_elast'] = de
+print("Elasticities:")
+print(df['dist_elast'].describe())
+print(df.groupby('hpiquartile')['dist_elast'].describe())
 
 
 ####################
 ####################
 ##################### 
-# plot objective function for different pi_init
+# plot objective function for different distance coefficients
 plot_obj = False
 if plot_obj:
     pyblp.options.verbose = False
 
     objective_log = []
-    dist_coefs = np.linspace(-5,2,15)
+    dist_coefs = np.linspace(-4,2,25)
     for bb in dist_coefs:
         beta_init = res.beta
         beta_init[res.beta_labels.index('log(dist)')] = bb
@@ -68,11 +71,10 @@ if plot_obj:
             res = problem.solve(beta = beta_init, sigma = res.sigma, optimization=pyblp.Optimization('return'))
         objective_log.append(res.objective[0][0])
         print(bb, res.objective[0][0])
-        sys.stdout.flush()
 
     # Plot
     fig, ax = plt.subplots()
-    ax.set_xlabel('pi_init')
+    ax.set_xlabel('distance coefficient')
     ax.set_ylabel('objective')
     ax.set_title('Objective function')
     ax.plot(dist_coefs, objective_log)
