e9174fb9f275dc300b04fe51b950123620ace05b
Li Zhijian <zhijianli9999@gmail.com>
2023-07-09 09:18:46 -0700

    jingyuan's changes

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Jul 21, 2022
+Created on Jul, 2022
 @author: Jingyuan Hu
 """
 
 import os
-os.chdir('/export/storage_covidvaccine/Code')
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 from utils.optimize_main import optimize_main
 from utils.evaluate_main import evaluate_main
+# from utils.partnership_summary import create_summary
 
 
 def main():
 
     '''
-    Model_list = ['MaxRateHPIDist', 'MaxRateDist', 'MaxRateFixV', 'MinDist']
+    Model_list = ['MaxVaxHPIDistBLP', 'MaxVaxDistBLP', 'MaxVaxHPIDistLogLin', 'MaxVaxDistLogLin', 'MaxVaxFixV', 'MinDist']
     Chain_list = ['Dollar', 'DiscountRetailers', 'Mcdonald', 'Coffee', 'ConvenienceStores', 'GasStations', 'CarDealers', 'PostOffices', 'HighSchools', 'Libraries']
     K_list = [8000, 10000, 12000]
+    M_list = [5, 10]
     Demand_estimation = 'BLP'
+    
     '''
     
-    # optimize_main(Model_list = ['MaxRateHPIDist', 'MaxRateDist'], Chain_list = ['Dollar'], Demand_estimation = 'BLP')
-    # optimize_main(Model_list = ['MaxRateHPIDist'], Chain_list = ['Dollar'], K_list = [10000], Demand_estimation = 'linear')
-    # optimize_main(Model_list = ['MaxRateDist'], Chain_list = ['Dollar'], K_list = [10000], Demand_estimation = 'BLP')
 
-    evaluate_main(Model_list = ['MaxRateDist'], Chain_list = ['Dollar'], K_list = [10000])
+    # optimize_main(Model_list = ['MaxVaxHPIDistBLP'], Chain_list = ['Dollar'], K_list = [10000], M_list = [5])
+
+    evaluate_main(Model_list = ['MaxVaxHPIDistBLP'], Chain_list = ['Dollar'], K_list = [10000], M_list = [5])
+
+    # create_summary(Model_list = ['MaxVaxHPIDistBLP'], Chain_list = ['Dollar'], K_list = [10000], M_list = [5], filename='NEW')
+
+
+
 
 
 if __name__ == "__main__":
