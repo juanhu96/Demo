0870d3f1aea0dde58cbb02f3eb88fcb5ff3ae424
juanhu96 <juanhu96@gmail.com>
2023-08-15 23:46:26 -0700

    new F matrix (temp)

--- temp.py
+++ temp.py
@@ -2,30 +2,100 @@
 # -*- coding: utf-8 -*-
 """
 Created on Jul, 2022
-Copied from Demand/aux_datamerge.py
+Modified from Demand/aux_datamerge.py
 """
 
 import numpy as np
 import pandas as pd
-try:
-    from demand_utils import demest_funcs as de
-except:
-    from Demand.demand_utils import demest_funcs as de
+from utils.import_dist import import_dist
+
+datadir = "/export/storage_covidvaccine/Data/"
+resultdir = '/export/storage_covidvaccine/Result/'
 
 
-datadir = "/export/storage_covidvaccine/Data"
+Chain_type = 'Dollar'
+M = 5
+Population, Quartile, p_current, p_total, pc_current, pc_total, C_total, Closest_current, Closest_total, c_currentMinDist, c_totalMinDist, num_tracts, num_current_stores, num_total_stores = import_dist(Chain_type, M)
+
+capacity = 10000
+
+tract = pd.read_csv(f'{datadir}tract_centroids.csv', delimiter = ",", dtype={'GEOID': int, 'POPULATION': int}) # tract info
+block = pd.read_csv(f"{datadir}Analysis/Demand/block_data.csv") # ['zip', 'blkid', 'dist', 'population', 'weights', 'market_ids', 'nodes', 'logdist']
+# block.sort_values(by=['blkid'], inplace=True)
+blk_tract = pd.read_csv(f"{datadir}Intermediate/blk_tract.csv", usecols=['tract', 'blkid']) # block-tract
+block_utils = pd.read_csv(f'{resultdir}Demand/agent_results_{capacity}_200_3q.csv', delimiter = ",") # block estimates
+distdf = pd.read_csv(f"{datadir}Intermediate/ca_blk_pharm_dist.csv", dtype={'locid': int, 'blkid': int}) # 'blkid', 'locid', 'logdist'
+
+
+### Current pharmacies
+C_current = C_total[:,0:num_current_stores]
+M_closest_current = np.argpartition(C_current, M, axis=1)[:,:M]
+F_current = np.zeros((num_tracts, num_current_stores))
+
+for i in range(num_tracts):
+
+    tract_id, tract_pop = tract['GEOID'][i], tract['POPULATION'][i]
+    tract_closest_sites = M_closest_current[i]
+
+    blocks_id = blk_tract[blk_tract.tract == tract_id].blkid.to_list()
+
+    for site in tract_closest_sites:
+
+        tract_site_willingess = 0
+
+        for block_id in blocks_id:
+                
+            block_pop = block[block.blkid == block_id].population.values[0]
+            block_abd, block_distcoef = block_utils[block_utils.blkid == block_id].abd.values[0], block_utils[block_utils.blkid == block_id].distcoef.values[0]
+            logdist = distdf[(distdf.blkid == block_id) & (distdf.locid == site)].logdist.values[0]
+
+            block_utility = block_abd + block_distcoef * logdist
+            tract_site_willingess += (block_pop / tract_pop) * np.exp(block_utility)/(1 + np.exp(block_utility))
+
+        print(f'New willingness {str(tract_site_willingess)}\n')
+
+        F_current[i][site] = tract_site_willingess
+
+
+### stores
+C_current = C_total[:,0:num_current_stores]
+M_closest_current = np.argpartition(C_current, M, axis=1)[:,:M]
+F_current = np.zeros((num_tracts, num_current_stores))
+
+for i in range(num_tracts):
+
+    tract_id, tract_pop = tract['GEOID'][i], tract['POPULATION'][i]
+    tract_closest_sites = M_closest_current[i]
+
+    blocks_id = blk_tract[blk_tract.tract == tract_id].blkid.to_list()
+
+    for site in tract_closest_sites:
+
+        tract_site_willingess = 0
+
+        for block_id in blocks_id:
+                
+            block_pop = block[block.blkid == block_id].population.values[0]
+            block_abd, block_distcoef = block_utils[block_utils.blkid == block_id].abd.values[0], block_utils[block_utils.blkid == block_id].distcoef.values[0]
+            logdist = distdf[(distdf.blkid == block_id) & (distdf.locid == site)].logdist.values[0]
+
+            block_utility = block_abd + block_distcoef * logdist
+            tract_site_willingess += (block_pop / tract_pop) * np.exp(block_utility)/(1 + np.exp(block_utility))
+
+        print(f'New willingness {str(tract_site_willingess)}\n')
+
+        F_current[i][site] = tract_site_willingess
 
 
 
-## ZIP-level data, produced by /Demand/datawork/zip/prep_zip.py
-df = pd.read_csv(f"{datadir}/Analysis/Demand/demest_data.csv")
-nsplits = 3 # number of HPI groups
-df = de.hpi_dist_terms(df, nsplits=nsplits, add_bins=True, add_dummies=True, add_dist=False)
 
-## block-ZIP crosswalk
-cw_pop = pd.read_csv(f"{datadir}/Analysis/Demand/block_data.csv") #produced by /Demand/datawork/block/prep_block.py
-print(cw_pop.columns.tolist())
-cw_pop.sort_values(by=['blkid'], inplace=True)
+
+##########################################################################################
+
+
+
+
+
 
 '''
 ## block-pharmacy distances (~500 rows per block, >1B rows)
